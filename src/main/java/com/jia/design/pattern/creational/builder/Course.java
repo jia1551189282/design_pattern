bc1b9560792e80707113a4514a2cd8277e63c2e2
zjiajia <1551189282@qq.com>
2019-06-18 23:18:38 +0800

    建造者模式

---
+++ src/main/java/com/jia/design/pattern/creational/builder/Course.java
@@ -0,0 +1,13 @@
+package com.jia.design.pattern.creational.builder;
+
+import lombok.Data;
+
+@Data
+public class Course {
+    private String courseName;
+    private String coursePPT;
+    private String coursevideo;
+    private String courseArticle;
+    //question & answer
+    private String courseQA;
+}
